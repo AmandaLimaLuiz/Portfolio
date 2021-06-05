72f1d16dc362502fff4f14d0927e8c65ab8a091b
AmandaLimaLuiz <delimaluiz.amanda@gmail.com>
2021-06-04 22:32:09 -0300

    Portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+var container = document.querySelector(".container");
+container.addEventListener("click", function(){
+    var container = document.querySelector(".container").classList.toggle("show-menu");
+});
+
+var counter=1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter>21){
+        counter=1;
+    }
+},5000);
